ef24de7013f622b247fe03e16b40207126a25f85
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-25 12:20:18 +0100

    loops through and outputs the jokes

--- app.js
+++ app.js
@@ -12,12 +12,16 @@ function getJokes(e) {
       const response = JSON.parse(this.responseText);
       
       let output = '';
-      
-      if(response.type === 'success') {
 
+      if(response.type === 'success') {
+        response.value.forEach(function(joke) {
+          output += `<li>${joke.joke}</li>`;
+        });
       } else {
         output += '<li>Something went wrong</li>'
       }
+
+      document.querySelector('.jokes').innerHTML = output;
     }
   }
 
